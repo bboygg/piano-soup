57452663f8fa384ad6247f1bfcf4316e5b2ff259
bboygg <bboygg94@gmail.com>
2026-06-14 01:08:19 +0900

    Config: Adjust for custom domain piano-soup.com

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: process.env.NODE_ENV === 'production' ? '/piano-soup' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/piano-soup/' : '',
   images: {
     unoptimized: true,
   },
